680fd2afac154878d1720bb69d4f3c024cba3081
Ludivine <sagnol.ludivine@gmail.com>
2023-06-26 11:44:36 +0200

    quest 2

--- src/main/java/com/wildcodeschool/doctor/controller/DoctorController.java
+++ src/main/java/com/wildcodeschool/doctor/controller/DoctorController.java
@@ -1,17 +1,26 @@
 package com.wildcodeschool.doctor.controller;
 
 import com.wildcodeschool.doctor.model.Doctor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.server.ResponseStatusException;
 
 @Controller
 public class DoctorController {
-
-    @GetMapping("/doctor/")
+    Doctor thirteen = new Doctor(13, "Jodie Whittaker");
+    @GetMapping("/doctor/{number}")
     @ResponseBody
-    public Doctor doctor() {
-
-        return null;
+    public Doctor doctor(@PathVariable int number) {
+        System.out.println(number);
+        if(number == 13) {
+            return thirteen;
+        } else if (number <=12 && number > 0) {
+            throw new ResponseStatusException(HttpStatus.SEE_OTHER);
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Impossible de récupérer l'incarnation " + number);
+        }
     }
 }
